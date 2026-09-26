a131148d2c013f6765e3b1f5f49b980b1ab07db0
unknown <anasbanat98banat@gmail.com>
2026-09-26 13:06:54 +0300

    edit import users

--- app/Http/Controllers/AdminManageUserController.php
+++ app/Http/Controllers/AdminManageUserController.php
@@ -9,6 +9,7 @@ use App\Models\BookingLocation;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\StreamedResponse;
